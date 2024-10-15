62d3b822d7cd0b5615f96f83b9e1cecbb7d818ff
YasarDogann <yasardgn99@gmail.com>
2024-10-15 18:01:41 +0300

    first_class

---
+++ patika_w4_firstClass/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace patika_w4_firstClass
+{
+    public class Person
+    {
+        private string name;
+        private string surname;
+        private DateTime birthDay;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Surname
+        {
+            get { return surname; }
+            set { surname = value; }
+        }
+
+        public DateTime BirthDay
+        {
+            get { return birthDay; }
+            set { birthDay = value; }
+        }
+
+
+        public void PersonInfo()
+        {
+            Console.WriteLine($"Adı: {name}\r\nSoyadı: {surname}\r\nDoğum Tarihi: {birthDay.ToString("dd.MM.yyyy")}");
+        }
+    }
+}
---
+++ patika_w4_firstClass/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace patika_w4_firstClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person teacher = new Person();
+
+            teacher.Name = "Okan";
+            teacher.Surname = "Buruk";
+            teacher.BirthDay = new DateTime(1993,02,19);
+
+            teacher.PersonInfo();
+
+            Console.WriteLine("---------------------------------");
+
+            Person student1 = new Person();
+
+            student1.Name = "Yaşar";
+            student1.Surname = "Doğan";
+            student1.BirthDay = new DateTime(2001, 04, 08);
+            student1.PersonInfo();
+
+            Console.WriteLine("---------------------------------");
+
+            Person student2 = new Person();
+            student2.Name = "Mauro";
+            student2.Surname = "Icardi";
+            student2.BirthDay = new DateTime(1993, 02, 019);
+            student2.PersonInfo();
+        }
+    }
+}
